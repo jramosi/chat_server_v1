7b10c12618675f2106ad5a060a1dbd8f918e3a7c
jramosi <jkevinramos59@gmail.com>
2024-03-04 16:54:54 -0400

    agregue el proyecto hecho

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuario actual: ${socket.id}`);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`Usuario con id: ${socket.id} se unio a la sala: ${data} `);
+  });
+  socket.on("send_message", (data) => {
+    console.log("Mensaje recibido");
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log(`Usuario desconectado`, socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("####################################");
+  console.log("######## SERVIDOR CORRIENDO ########");
+  console.log("####################################");
+});
