10412d215cf05896e9f31c505122d38598cdc165
jramosi <jkevinramos59@gmail.com>
2024-03-04 18:45:52 -0400

    agregue constantes

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
+const dotenv = require("dotenv");
+dotenv.config();
+const URL = process.env.URL_FRONTEND;
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: URL,
     methods: ["GET", "POST"],
   },
 });
